daed3c6e8c6eb6fa3cf04f180a6f621b315c346c
Dickson Chiu <dickson.chiu8@gmail.com>
2022-04-06 16:59:14 -0600

    Updated dog_data json template and added sql files

---
+++ chiu.dickson/data/users_data.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS `track_202230_users` (
+`id` INT NULL,
+`name` VARCHAR(MAX) NULL,
+`username` VARCHAR(MAX) NULL,
+`email` VARCHAR(MAX) NULL,
+`password` VARCHAR(MAX) NULL,
+`age` INT NULL,
+`description` VARCHAR(MAX) NULL,
+`img` VARCHAR(MAX) NULL,
+`date_create` VARCHAR(MAX) NULL
+);
+
+INSERT INTO track_202230_users VALUES
+(1,'Alvarado Carney','user1','user1@gmail.com',md5('pass'),31,'Ex qui aute est Lorem eu. Consectetur eiusmod velit veniam nostrud. Lorem ad excepteur amet do in eu fugiat ut do.','https://via.placeholder.com/400/983/fff/?text=user1','2022-01-11 10:07:17'),
+(2,'Barr Burnett','user2','user2@gmail.com',md5('pass'),57,'Elit nulla nisi voluptate commodo Lorem minim. Non tempor tempor quis pariatur. Officia eiusmod officia minim aute esse duis enim duis eu proident proident.','https://via.placeholder.com/400/773/fff/?text=user2','2022-02-21 05:09:12'),
+(3,'Gladys Cleveland','user3','user3@gmail.com',md5('pass'),15,'Lorem ad ullamco cillum fugiat duis ex. Ea consequat veniam enim pariatur enim voluptate elit nulla nostrud officia labore ullamco. Excepteur sint eu irure voluptate in aliqua sit laboris.','https://via.placeholder.com/400/947/fff/?text=user3','2022-03-03 03:40:13'),
+(4,'Renee Brewer','user4','user4@gmail.com',md5('pass'),57,'Dolore sint occaecat tempor esse non Lorem labore eiusmod non. Excepteur do duis do sunt amet nulla sit voluptate magna. Sunt dolore cillum tempor dolor Lorem laborum ex non id id nulla id ea.','https://via.placeholder.com/400/759/fff/?text=user4','2022-04-05 05:45:30'),
+(5,'Georgette Graham','user5','user5@gmail.com',md5('pass'),23,'Nisi consequat eiusmod aliquip aliquip cillum sit et irure. Officia labore tempor labore et dolor labore ipsum adipisicing Lorem est et reprehenderit. Et tempor velit ex et tempor ipsum veniam exercitation commodo.','https://via.placeholder.com/400/833/fff/?text=user5','2022-02-17 02:31:35'),
+(6,'Clemons Carroll','user6','user6@gmail.com',md5('pass'),56,'Enim ut voluptate exercitation Lorem. Officia amet consequat sit occaecat veniam. Sit consectetur elit sunt ad officia ullamco laborum proident nisi non id.','https://via.placeholder.com/400/966/fff/?text=user6','2022-03-10 09:02:24'),
+(7,'Tanya Paul','user7','user7@gmail.com',md5('pass'),31,'Aliqua irure aliquip sunt incididunt esse proident nostrud nisi cillum ullamco. Reprehenderit anim cillum pariatur ex proident incididunt anim in tempor sint qui duis. Eiusmod tempor amet labore qui quis.','https://via.placeholder.com/400/762/fff/?text=user7','2022-02-05 09:29:51'),
+(8,'Bradshaw Mercer','user8','user8@gmail.com',md5('pass'),41,'Officia voluptate id duis aliqua elit nostrud amet reprehenderit nisi. Ex cupidatat ullamco sint cupidatat duis veniam elit. Cillum duis est anim cupidatat ex ea.','https://via.placeholder.com/400/740/fff/?text=user8','2022-01-12 03:09:27'),
+(9,'Angelique Lester','user9','user9@gmail.com',md5('pass'),24,'Aute deserunt pariatur cupidatat commodo eu officia laborum anim fugiat eu duis aliqua dolore nulla. Ipsum culpa pariatur pariatur magna laboris. Et ullamco tempor ut aliqua.','https://via.placeholder.com/400/933/fff/?text=user9','2022-03-08 02:23:23'),
+(10,'Ruby Henry','user10','user10@gmail.com',md5('pass'),59,'Ipsum irure veniam est magna amet aute incididunt eu veniam dolor dolore labore adipisicing. Laboris consectetur et duis ad laborum exercitation deserunt aliquip pariatur. Incididunt aliquip dolore aute id est est aute non labore nostrud mollit dolore cillum.','https://via.placeholder.com/400/708/fff/?text=user10','2022-01-29 12:02:49');
