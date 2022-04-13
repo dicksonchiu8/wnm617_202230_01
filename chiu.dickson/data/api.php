9cf32635b63793a880dd969252c168253025a7ae
Dickson Chiu <dickson.chiu8@gmail.com>
2022-04-12 22:56:58 -0600

    Updated api.php and index files

--- chiu.dickson/data/api.php
+++ chiu.dickson/data/api.php
@@ -46,18 +46,54 @@ function makeQuery($c,$ps,$p,$makeResults=true) {
    }
 }
 
+function makeStatement($data){
+    $c = makeConn();
+    $t = $data->type;
+    $p = $data->params;
+    
+    switch($t){
+        case "users_all":
+            return makeQuery($c, "SELECT * FROM `track_202230_users`", $p);
+        case "dogs_all":
+            return makeQuery($c, "SELECT * FROM `track_202230_dogs`", $p);
+        case "locations_all":
+            return makeQuery($c, "SELECT * FROM `track_202230_locations`", $p);    
+            
+        case "user_by_id":
+            return makeQuery($c, "SELECT * FROM `track_202230_users` WHERE `id`= ?", $p);
+        case "dog_by_id":
+            return makeQuery($c, "SELECT * FROM `track_202230_dogs` WHERE `id`= ?", $p);
+        case "location_by_id":
+            return makeQuery($c, "SELECT * FROM `track_202230_locations` WHERE `id`= ?", $p);
+            
+        case "dogs_by_user_id":
+            return makeQuery($c, "SELECT * FROM `track_202230_dogs` WHERE `user_id`= ?", $p);
+        case "locations_by_user_id":
+            return makeQuery($c, "SELECT * FROM `track_202230_locations` WHERE `user_id`= ?", $p);
+        
+        case "check_signin":
+            return makeQuery($c, "SELECT id from `track_202230_users` WHERE `username` = ? AND `password` = md5(?)", $p);
+        
+        default:
+            return ["error"=>"No Matched Type"];
+    }
+}
+
 /*
 "SELECT * FROM track_202230_users",
 "SELECT * FROM track_202230_users WHERE id = ?",
 "SELECT * FROM track_202230_dogs WHERE user_id = ?",
-*/
 
-die(
-   json_encode(
       makeQuery(
          makeConn(),
          "SELECT * FROM track_202230_dogs WHERE user_id = ?",
          [3]
       )
-   )
+*/
+
+$data = json_decode(file_get_contents("php://input"));
+
+echo json_encode(
+    makeStatement($data),
+    JSON_NUMERIC_CHECK
 );
