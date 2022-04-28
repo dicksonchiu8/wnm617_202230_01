e405fcfca6db7e097fd586b846c6cbdf9036b3f4
Dickson Chiu <dickson.chiu8@gmail.com>
2022-04-27 22:29:16 -0600

    Updated forms js file to add validation on add dog page

--- chiu.dickson/lib/js/forms.js
+++ chiu.dickson/lib/js/forms.js
@@ -3,7 +3,43 @@ const submitAnimalAdd = () => {
     let breed = $("#dog-breed-select :selected").text();
     let description = $("#dog-description").val();
    
-    console.log({name,breed,description});
+   console.log("GOT HERE");
+    if(name.length < 1 && breed === "Select a dog breed"){
+        $("#add-dog-error-message").html("Please Enter the Dog's Name.<br>Please Pick a Dog Breed.");
+          $("#add-dog-error-message").addClass("visible");
+          $("#add-dog-error-message").removeClass("no-display");
+         
+          // remove error message after some time
+          setTimeout(function() {
+            $("#add-dog-error-message").addClass("no-display");
+            $("#add-dog-error-message").removeClass("visible");
+          }, 4000);         
+    } else if(name.length < 1 ){
+        $("#add-dog-error-message").html("Please Enter the Dog's Name.");
+          $("#add-dog-error-message").addClass("visible");
+          $("#add-dog-error-message").removeClass("no-display");
+         
+          // remove error message after some time
+          setTimeout(function() {
+            $("#add-dog-error-message").addClass("no-display");
+            $("#add-dog-error-message").removeClass("visible");
+          }, 4000);           
+    } else if(breed === "Select a dog breed"){
+        $("#add-dog-error-message").html("Please Pick a Dog Breed.");
+          $("#add-dog-error-message").addClass("visible");
+          $("#add-dog-error-message").removeClass("no-display");
+         
+          // remove error message after some time
+          setTimeout(function() {
+            $("#add-dog-error-message").addClass("no-display");
+            $("#add-dog-error-message").removeClass("visible");
+          }, 4000);           
+    } else{
+        console.log({name,breed,description});
+        sessionStorage.animalId = $(this).data('id');
+        $.mobile.navigate("#add-dog-location-page")
+    }
+    
 }
 
 const submitAnimalEdit = () => {
