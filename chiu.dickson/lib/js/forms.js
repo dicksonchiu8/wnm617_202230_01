ff20d6e63a73db242040f05e7dfb6e5125a77f36
Dickson Chiu <dickson.chiu8@gmail.com>
2022-04-27 22:43:56 -0600

    Updated forms js file for form validation on edit dog page

--- chiu.dickson/lib/js/forms.js
+++ chiu.dickson/lib/js/forms.js
@@ -3,7 +3,7 @@ const submitAnimalAdd = () => {
     let breed = $("#dog-breed-select :selected").text();
     let description = $("#dog-description").val();
    
-   console.log("GOT HERE");
+   //FORM VALIDATION
     if(name.length < 1 && breed === "Select a dog breed"){
         $("#add-dog-error-message").html("Please Enter the Dog's Name.<br>Please Pick a Dog Breed.");
           $("#add-dog-error-message").addClass("visible");
@@ -46,8 +46,43 @@ const submitAnimalEdit = () => {
     let name = $("#edit-dog-fullname").val();
     let breed = $("#edit-dog-breed-select :selected").text();
     let description = $("#edit-dog-description").val();
-   
-    console.log({name,breed,description});
+  
+   //FORM VALIDATION
+    if(name.length < 1 && breed === "Select a dog breed"){
+        $("#edit-dog-error-message").html("Dog's Name can't be Empty.<br>Please Pick a Dog Breed.");
+          $("#edit-dog-error-message").addClass("visible");
+          $("#edit-dog-error-message").removeClass("no-display");
+         
+          // remove error message after some time
+          setTimeout(function() {
+            $("#edit-dog-error-message").addClass("no-display");
+            $("#edit-dog-error-message").removeClass("visible");
+          }, 4000);         
+    } else if(name.length < 1 ){
+        $("#edit-dog-error-message").html("Dog's Name can't be Empty.");
+          $("#edit-dog-error-message").addClass("visible");
+          $("#edit-dog-error-message").removeClass("no-display");
+         
+          // remove error message after some time
+          setTimeout(function() {
+            $("#edit-dog-error-message").addClass("no-display");
+            $("#edit-dog-error-message").removeClass("visible");
+          }, 4000);           
+    } else if(breed === "Select a dog breed"){
+        $("#edit-dog-error-message").html("Please Pick a Dog Breed.");
+          $("#edit-dog-error-message").addClass("visible");
+          $("#edit-dog-error-message").removeClass("no-display");
+         
+          // remove error message after some time
+          setTimeout(function() {
+            $("#edit-dog-error-message").addClass("no-display");
+            $("#edit-dog-error-message").removeClass("visible");
+          }, 4000);           
+    } else{
+        console.log({name,breed,description});
+        sessionStorage.animalId = $(this).data('id');
+        $.mobile.navigate("#dog-profile-page-info")
+    }   
 }
 
 const submitUserAdd = () => {
